a8d41126400bb26a818dc93c04d774f2cb813bdf
Julien Micheletti <j.micheletti@orange.fr>
2018-02-18 12:46:20 -0800

    partie 1

--- src/modele/SeamCarving.java
+++ src/modele/SeamCarving.java
@@ -87,16 +87,16 @@ public class SeamCarving {
 							graph.addEdge(new Edge(count, count + largeur, itr[i][j]));
 						}
 					}
-				}else{
+				} else {
 					graph.addEdge(new Edge(count, n - 1, itr[hauteur - 1][j]));
 				}
 				count++;
 			}
 			counthaut++;
-			if (passe == true){
-			 	i--;
-			 	passe = false;
-			 }
+			if (passe == true) {
+				i--;
+				passe = false;
+			}
 		}
 		graph.writeFile("test.dot");
 		return graph;
@@ -186,36 +186,44 @@ public class SeamCarving {
 		return newTab;
 	}
 
-	public static int coutChemin(ArrayList<Edge> chemin){
+	public static int coutChemin(ArrayList<Edge> chemin) {
 		int result = 0;
-		for (int i = 0; i < chemin.size(); i++){
+		for (int i = 0; i < chemin.size(); i++) {
 			result += chemin.get(i).cost;
 		}
 		return result;
 	}
-	public static ArrayList<Edge> twograph(Graph g, int s, int t){
+
+	public static HashMap<String, ArrayList<Edge>> twograph(Graph g, int s, int t) {
 		ArrayList<Edge> chemin = djikstra(g, s, t);
 		ArrayList<Edge> temp = new ArrayList<Edge>();
-		Graph copyGraph = new Graph(g);
-		int res;
+		ArrayList cout = new ArrayList();
 		Iterator<Edge> iterator = g.edges().iterator();
-		int resultat[] = new int[g.getEdge()*2];
+		HashMap<String, ArrayList<Edge>> map = new HashMap<String, ArrayList<Edge>>();
+		int res;
+		int resultat[] = new int[g.getEdge() * 2];
 		int i = 0;
+		int compare = 0;
 		int from;
 		int to;
 
-		while (iterator.hasNext()){
+		for (int p = 0; p < g.vertices(); p++){
+			cout.add(coutChemin(djikstra(g, 0, p)));
+		}
+		while (iterator.hasNext()) {
+		//	System.out.println("ok");
 			Edge edge = iterator.next();
 			temp.add(edge);
 			//System.out.println("Arrete " + edge.from + " - " + edge.to + " Ancien cout = " + edge.cost + " Operation -> " + coutChemin(djikstra(g, 0, edge.from)) + " - " + coutChemin(djikstra(g, 0, edge.to)));
+		//	System.out.println(edge.from);
 			res = edge.cost + coutChemin(djikstra(g, 0, edge.from)) - coutChemin(djikstra(g, 0, edge.to));
 			resultat[i] = res;
 			i++;
 		}
-		for (int j = 0; j < temp.size(); j++){
+		for (int j = 0; j < temp.size(); j++) {
 			temp.get(j).setEdge(resultat[j]);
 		}
-		for (int k = 0; k < chemin.size(); k++){
+		for (int k = 0; k < chemin.size(); k++) {
 			Edge edge = chemin.get(k);
 			from = edge.from;
 			to = edge.to;
@@ -223,26 +231,59 @@ public class SeamCarving {
 			edge.setTo(from);
 		}
 		ArrayList<Edge> chemin2 = djikstra(g, s, t);
-		for (int m = 0; m < chemin2.size(); m++){
-			System.out.println(chemin2.get(m).from);
+		for (int k = 0; k < chemin.size(); k++) {
+			Edge edge = chemin.get(k);
+			from = edge.from;
+			to = edge.to;
+			edge.setFrom(to);
+			edge.setTo(from);
 		}
-		g.writeFile("test");
-		return chemin;
+		for (int k = 0; k < chemin.size(); k++) {
+			for (int l = 0; l < chemin2.size(); l++) {
+				if (chemin.get(k).equals(chemin2.get(l))) {
+					chemin.remove(chemin.get(k));
+					chemin2.remove(chemin2.get(l));
+				}
+			}
+		}
+		/**for (int m = 0; m < chemin2.size(); m++){
+		 System.out.println("Arrete " + chemin2.get(m).from + " - " + chemin2.get(m).to + " Ancien cout = " + chemin2.get(m).cost);
+
+		 }
+		 for (int m = 0; m < chemin.size(); m++){
+		 System.out.println("Arrete " + chemin.get(m).from + " - " + chemin.get(m).to + " Ancien cout = " + chemin.get(m).cost);
+
+		 }**/
+
+		map.put("Chemin1", chemin);
+		map.put("Chemin2", chemin2);
+		return map;
 
 	}
 
-
 	public static void main(String args[]) {
+		long debut = System.currentTimeMillis();
+		ArrayList<Edge> chemin;
+		ArrayList<Edge> chemin2;
 		int test[][] = { {3, 11, 24, 39},{8, 21, 29, 39}, {200, 60, 25, 0} };
+		//int image[][] = readpgm("ex3.pgm");
 		int itr[][] = interest(test);
 		Graph g = tograph(itr);
-		ArrayList<Edge> chemin = djikstra(g, 0, 8);
+		HashMap<String, ArrayList<Edge>> map = twograph(g, 0, g.vertices() - 1);
+		System.out.println(System.currentTimeMillis() - debut);
+		chemin = map.get("Chemin1");
+		chemin2 = map.get("Chemin2");
+		//image = supprChemin(image, chemin);
+		//image = supprChemin(image, chemin2);
+		//writepgm(image, "TestFinal3");
+	}
+}
+		//itr = interest(image);
+		//ArrayList<Edge> chemin = djikstra(g, 0, 8);
 		//System.out.println(coutChemin(chemin));
 		//for (int i = 0; i < chemin.size(); i++){
 			//System.out.println(chemin.get(i).cost);
 		//}
-		twograph(g, 0, g.vertices() - 1);
-		//long debut = System.currentTimeMillis();
 		//int image[][] = readpgm("ex3.pgm");
 		//int itr[][] = interest(image);
 		//Graph g;
@@ -254,7 +295,4 @@ public class SeamCarving {
 			itr = interest(image);
 			i++;
 		}
-		writepgm(image, "TestFinal2");
-		System.out.println(System.currentTimeMillis()-debut);**/
-	}
-}
+	}**/
